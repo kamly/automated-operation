efbe29d1025b96b1d028b11f7958dc18dffb5019
kamly <121368588@qq.com>
2018-02-11 19:15:36 +0800

    修复重置密码错误

--- reset_redis_pwd.sh
+++ reset_redis_pwd.sh
@@ -10,11 +10,13 @@ reset_redis_pwd(){
 	read -p "Please input New Redis root password (default:root) : " redis_root_pass
 	redis_root_pass=${redis_root_pass:=root} # 提供默认      
 
-    # 修改密码
-    sed -i "s@requirepass@requirepass ${redis_root_pass}@g" $redis_install_dir/etc/${redis_port}.conf
+    service redis stop # 停止服务
 
-	service redis restart
-    
+    # 修改密码
+    sed -i "s@requirepass@requirepass ${redis_root_pass}@g" $redis_install_dir/etc/${redis_port}.conf  # 登录密码 配置文件
+    sed -i "s@PASSWORD=root@PASSWORD=${redis_root_pass}@g" /etc/init.d/redis  # 登录密码 脚本服务
+
+	service redis start # 启动服务
     if [ $? == 0 ]; then
 		echo -e "New Redis server root password is \033[41m $redis_root_pass \033[0m"		
 	else
