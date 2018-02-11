322654e2caa58804c51f6c047d585fb462090ac3
kamly <121368588@qq.com>
2018-02-11 20:34:07 +0800

    优化redis更改密码

--- reset_redis_pwd.sh
+++ reset_redis_pwd.sh
@@ -7,12 +7,18 @@
 
 reset_redis_pwd(){
 
-	read -p "Please input New Redis root password (default:root) : " redis_root_pass
-	redis_root_pass=${redis_root_pass:=root} # 提供默认      
+    read -p "Please input Old Redis root password (default:root) : " redis_old_pass
+	redis_old_pass=${redis_old_pass:=root} # 提供默认   
 
+	read -p "Please input New Redis root password (default:root) : " redis_root_pass
+	redis_root_pass=${redis_root_pass:=root} # 提供默认        
+
+    # 在内部进行修改
+    $redis_install_dir/src/redis-cli -h 127.0.0.1 -p ${redis_port} -a "${redis_old_pass}" config set requirepass ${redis_root_pass}
+
+    # 在外部修改
     service redis stop # 停止服务
 
-    # 修改密码
     sed -i "s/^requirepass.*$/requirepass ${redis_root_pass}/g" $redis_install_dir/etc/${redis_port}.conf  # 登录密码 配置文件
     sed -i "s/^PASSWORD.*$/PASSWORD=${redis_root_pass}/g" /etc/init.d/redis  # 登录密码 脚本服务
     systemctl daemon-reload
