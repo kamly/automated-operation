68ea4cd54eca4d20fd6b4982ff97de6fcd48526e
kamly <121368588@qq.com>
2018-02-11 13:08:20 +0800

    刷新ldconfig

--- uninstall.sh
+++ uninstall.sh
@@ -190,7 +190,8 @@ uninstall_mysql(){
 
 			# 使用ldconfig命令将/usr/local/mysql加入到默认库
 			rm -rf /etc/ld.so.conf.d/mysql.conf
-
+			ldconfig 
+			
 			echo "Uninstall mysql successful!"
 		else
 			echo
