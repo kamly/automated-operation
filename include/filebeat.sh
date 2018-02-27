d050868683beaa67ff3592ec96afcb0ff06fe9f7
kamly <121368588@qq.com>
2018-02-27 13:23:06 +0800

    切换

--- include/filebeat.sh
+++ include/filebeat.sh
@@ -8,4 +8,6 @@ tar -zxvf $filebeat_gz
 # 移动目录 + 重命名
 mv $filebeat_version $filebeat_install_dir 
 
+popd  # 切换
+
 # 只进行安装，配置文件自行配置
--- include/logstash.sh
+++ include/logstash.sh
@@ -8,6 +8,8 @@ tar -zxvf $logstash_install_dir
 # 移动目录 + 重命名
 mv $logstash_version $logstash_install_dir 
 
+popd  # 切换
+
 # 只进行安装，配置文件自行配置
 
 
