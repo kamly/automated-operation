c63e541080b06be8610e4b788f310530e2244b01
kamly <121368588@qq.com>
2018-03-22 13:14:05 +0800

    [u] 重置

--- reset/reset_mysql_pwd.sh
+++ reset/reset_mysql_pwd.sh
@@ -4,7 +4,6 @@ clear
 
 . ../include/common.sh # 引入常量文件
 . ../include/public.sh # 引入公用函数
-
 . ../include/sysinfo.sh # 输出系统信息
 
 reset_mysql_pwd(){
@@ -18,9 +17,9 @@ reset_mysql_pwd(){
 	${mysql_install_dir}/bin/mysqladmin -u${mysql_enter_user} -P${mysql_port} -p"${mysql_old_pass}" password "${mysql_root_pass}"
 
 	if [ $? == 0 ]; then
-		echo -e "New Mysql server root password is \033[41m $mysql_root_pass \033[0m"	
+		echo -e "New Mysql server root password is \033[41m ${mysql_root_pass} \033[0m"	
 	else
-		echo -e "fail"	
+		echo -e "reset Mysql server root password fail"	
 	fi
 }
 reset_mysql_pwd
--- reset/reset_redis_pwd.sh
+++ reset/reset_redis_pwd.sh
@@ -2,7 +2,6 @@
 
 . ../include/common.sh # 引入常量文件
 . ../include/public.sh # 引入公用函数
-
 . ../include/sysinfo.sh # 输出系统信息
 
 reset_redis_pwd(){
@@ -19,15 +18,15 @@ reset_redis_pwd(){
     # 在外部修改
     service redis stop # 停止服务
 
-    sed -i "s/^requirepass.*$/requirepass ${redis_root_pass}/g" $redis_install_dir/etc/${redis_port}.conf  # 登录密码 配置文件
+    sed -i "s/^requirepass.*$/requirepass ${redis_root_pass}/g" ${redis_install_dir}/etc/${redis_port}.conf  # 登录密码 配置文件
     sed -i "s/^PASSWORD.*$/PASSWORD=${redis_root_pass}/g" /etc/init.d/redis  # 登录密码 脚本服务
     systemctl daemon-reload
 
 	service redis start # 启动服务
     if [ $? == 0 ]; then
-		echo -e "New Redis server root password is \033[41m $redis_root_pass \033[0m"		
+		echo -e "New Redis server root password is \033[41m ${redis_root_pass} \033[0m"		
 	else
-		echo -e "fail"	
+		echo -e "reset Redis server root password fail"	
 	fi
 }
 reset_redis_pwd
